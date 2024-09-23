f65df2a25021c704e8f901b7d1efcf169c931863
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-23 22:06:07 +0800

    Rebase from 'upstream'

--- ros_gz_bridge/ros_gz_bridge/mappings.py
+++ ros_gz_bridge/ros_gz_bridge/mappings.py
@@ -47,6 +47,9 @@ MAPPINGS = {
         Mapping('WrenchStamped', 'Wrench'),
         Mapping('Vector3', 'Vector3d'),
     ],
+    'gps_msgs': [
+        Mapping('GPSFix', 'NavSat'),
+    ],
     'nav_msgs': [
         Mapping('Odometry', 'Odometry'),
         Mapping('Odometry', 'OdometryWithCovariance'),
@@ -59,6 +62,7 @@ MAPPINGS = {
         Mapping('Contact', 'Contact'),
         Mapping('Contacts', 'Contacts'),
         Mapping('Entity', 'Entity'),
+        Mapping('EntityWrench', 'EntityWrench'),
         Mapping('Float32Array', 'Float_V'),
         Mapping('GuiCamera', 'GUICamera'),
         Mapping('JointWrench', 'JointWrench'),
